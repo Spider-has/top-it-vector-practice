9c6010739c5271dce413bb94e5ee9d05a56e976f
Spider-has <romanh998@gmail.com>
2026-03-17 11:12:43 +0000

    add top_it_vector constructor and destructor

--- top_it_vector.hpp
+++ top_it_vector.hpp
@@ -3,14 +3,27 @@
 
 #include <cstddef>
 
-namespace khasnulin
+namespace topit
 {
   template <class T> struct Vector
   {
-    T *data;
-    size_t size, cap;
+  public:
+    ~Vector();
+    Vector() noexcept;
+
+  private:
+    T *data_;
+    size_t size_, cap_;
   };
 
+  template <class T> Vector<T>::~Vector()
+  {
+    delete[] data_;
+  }
+
+  template <class T> Vector<T>::Vector() noexcept : data_(nullptr), size_(0), cap_(0)
+  {
+  }
 }
 
 #endif
