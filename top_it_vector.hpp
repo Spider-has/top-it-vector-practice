e7e0632a7ebf7f8d27f1d2594e468027b4226a35
Spider-has <romanh998@gmail.com>
2026-03-17 10:44:47 +0000

    vector pod

---
+++ top_it_vector.hpp
@@ -0,0 +1,16 @@
+#ifndef TOP_IT_VECTOR_HPP
+#define TOP_IT_VECTOR_HPP
+
+#include <cstddef>
+
+namespace khasnulin
+{
+  template <class T> struct Vector
+  {
+    T *data;
+    size_t size, cap;
+  };
+
+}
+
+#endif
