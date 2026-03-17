6e5fb14b65d2001c13e6e1faadaf2f6af1fde79d
Spider-has <romanh998@gmail.com>
2026-03-17 11:12:13 +0000

    add self test framework

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
+#include <iomanip>
+#include <ios>
+#include <iostream>
 
 #include "top_it_vector.hpp"
 
+bool testEmptyVector()
+{
+  return false;
+}
+
 int main()
 {
+  using f_p = std::pair<const char *, bool (*)()>;
+  f_p tests[] = {{"empty vector test", testEmptyVector}};
+
+  const size_t count = sizeof(tests) / sizeof(f_p);
+  std::cout << std::boolalpha;
+  size_t success = 0;
+  for (size_t i = 0; i < count; ++i)
+  {
+    std::cout << tests[i].first << ": " << (success += tests[i].second()) << "\n";
+  }
+  std::cout << "\ntests passed: " << success << " / " << count << "\n";
 }
